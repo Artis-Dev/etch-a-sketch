7b89f81445de65beaeb8d48893b7914b38bfc90b
Artis-Dev <admin@moonart.lt>
2020-05-22 16:20:29 +0300

    Add simple stylized game container with generateGrid function

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 function generateGrid() {
-  console.log('Hello World');
+  const gameContainer = document.getElementById('game-container');
+
+  const small = 16 * 22;
+  const medium = 32 * 44;
+  const big = 64 * 88;
+
+  for (let i = 0; i < medium; i++) {
+    const div = document.createElement('div');
+    gameContainer.appendChild(div);
+  }
 }
 
 generateGrid();
