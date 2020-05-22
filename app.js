d431d296d77b1eff11301e9145110fca657b22d5
Artis-Dev <admin@moonart.lt>
2020-05-22 20:32:31 +0300

    Add grid size options

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-function generateGrid() {
+function generateGrid(size = 32 * 44, cssClass = 'medium-grid-default') {
   const gameContainer = document.getElementById('game-container');
 
-  const small = 16 * 22;
-  const medium = 32 * 44;
-  const big = 64 * 88;
+  gameContainer.innerHTML = '';
 
-  for (let i = 0; i < medium; i++) {
+  gameContainer.classList.remove('small-grid', 'medium-grid-default', 'big-grid');
+  gameContainer.classList.add(cssClass);
+
+  for (let i = 0; i < size; i++) {
     const div = document.createElement('div');
     gameContainer.appendChild(div);
   }
 }
 
-generateGrid();
+function chooseSize() {
+  const sizeButtons = document.querySelectorAll('.size');
+
+  const small = 16 * 22;
+  const medium = 32 * 44;
+  const big = 64 * 88;
+
+  sizeButtons.forEach((selection) => {
+    selection.addEventListener('click', () => {
+      if (selection.classList.contains('small')) {
+        generateGrid(small, 'small-grid');
+      } else if (selection.classList.contains('medium')) {
+        generateGrid(medium, 'medium-grid-default');
+      } else {
+        generateGrid(big, 'big-grid');
+      }
+    });
+  });
+}
+
+function startGame() {
+  generateGrid();
+  chooseSize();
+}
+
+startGame();
