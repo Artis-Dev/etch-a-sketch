3465e37463502f42d7507e8fdde879239647c5d2
Artis-Dev <admin@moonart.lt>
2020-05-28 21:05:30 +0300

    Update design, add very simple responsivness

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function generateGrid(size = 32 * 44, cssClass = 'medium-grid-default') {
   gameContainer.classList.remove('small-grid', 'medium-grid-default', 'big-grid');
   gameContainer.classList.add(cssClass);
 
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size; i += 1) {
     const div = document.createElement('div');
     gameContainer.appendChild(div);
   }
@@ -19,14 +19,26 @@ function chooseSize() {
   const medium = 32 * 44;
   const big = 64 * 88;
 
+  sizeButtons[1].classList.add('active-button');
+
+  function selectButton(button) {
+    sizeButtons.forEach((selection) => {
+      selection.classList.remove('active-button');
+    });
+    button.classList.add('active-button');
+  }
+
   sizeButtons.forEach((selection) => {
     selection.addEventListener('click', () => {
       if (selection.classList.contains('small')) {
         generateGrid(small, 'small-grid');
+        selectButton(selection);
       } else if (selection.classList.contains('medium')) {
         generateGrid(medium, 'medium-grid-default');
+        selectButton(selection);
       } else {
         generateGrid(big, 'big-grid');
+        selectButton(selection);
       }
     });
   });
