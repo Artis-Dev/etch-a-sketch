2e074d6496ff52cd90f8eab907c97e0a09c54588
Artis-Dev <admin@moonart.lt>
2020-05-29 17:27:59 +0300

    Add Classic mode painting, UI tweaks

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const sizeButtons = document.querySelectorAll('.size');
+const modeButtons = document.querySelectorAll('.mode');
+
 function generateGrid(size = 32 * 44, cssClass = 'medium-grid-default') {
-  const gameContainer = document.getElementById('game-container');
+  const gameContainer = document.getElementById('grid-container');
 
   gameContainer.innerHTML = '';
 
@@ -12,32 +15,68 @@ function generateGrid(size = 32 * 44, cssClass = 'medium-grid-default') {
   }
 }
 
-function chooseSize() {
-  const sizeButtons = document.querySelectorAll('.size');
+function startPainting() {
+  const gridItems = document.querySelectorAll('#grid-container > div');
 
+  gridItems.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      item.classList.add('classic-paint');
+    });
+  });
+}
+
+function selectButton(button) {
+  if (button.classList.contains('mode')) {
+    modeButtons.forEach((selection) => {
+      selection.classList.remove('active-button');
+    });
+  } else {
+    sizeButtons.forEach((selection) => {
+      selection.classList.remove('active-button');
+    });
+  }
+  button.classList.add('active-button');
+}
+
+function changeSize() {
   const small = 16 * 22;
   const medium = 32 * 44;
   const big = 64 * 88;
 
   sizeButtons[1].classList.add('active-button');
 
-  function selectButton(button) {
-    sizeButtons.forEach((selection) => {
-      selection.classList.remove('active-button');
-    });
-    button.classList.add('active-button');
-  }
-
   sizeButtons.forEach((selection) => {
     selection.addEventListener('click', () => {
       if (selection.classList.contains('small')) {
         generateGrid(small, 'small-grid');
+        startPainting();
         selectButton(selection);
       } else if (selection.classList.contains('medium')) {
         generateGrid(medium, 'medium-grid-default');
+        startPainting();
         selectButton(selection);
       } else {
         generateGrid(big, 'big-grid');
+        startPainting();
+        selectButton(selection);
+      }
+    });
+  });
+}
+
+function changeMode() {
+  modeButtons[0].classList.add('active-button');
+
+  modeButtons.forEach((selection) => {
+    selection.addEventListener('click', () => {
+      if (selection.classList.contains('small')) {
+        startPainting();
+        selectButton(selection);
+      } else if (selection.classList.contains('medium')) {
+        startPainting();
+        selectButton(selection);
+      } else {
+        startPainting();
         selectButton(selection);
       }
     });
@@ -46,7 +85,10 @@ function chooseSize() {
 
 function startGame() {
   generateGrid();
-  chooseSize();
+  startPainting();
+  changeSize();
+  changeMode();
+  // erase();
 }
 
 startGame();
